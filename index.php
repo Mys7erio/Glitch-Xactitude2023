166049a9c712b669b8485b6bdc7dceb1ac503833
Shakir <shakirali.sacube@gmail.com>
2023-01-18 20:55:06 +0530

    Fixed bug in index.php which was causing login error

--- index.php
+++ index.php
@@ -1,4 +1,35 @@
 <!DOCTYPE html>
+  <?php
+  // PHP CODE FOR LOGIN VALIDATION
+    if (isset($_POST['submit'])) {
+      require('essentials/_conn.php');
+
+      $email=$_POST['email'];
+      $pass=$_POST['pass'];
+
+      $query="SELECT * from login where email='$email' and pass='$pass' and type ='0'";
+      $res=mysqli_query($conn, $query);
+
+      if ($res) {
+        if(mysqli_num_rows($res)>0)
+        {
+          $row = mysqli_fetch_assoc($res);
+          $name=$row['name'];
+          $id=$row['id'];
+          $type=$row['type'];
+          session_start();
+          $_SESSION['loggedin']=true;
+          $_SESSION['name']=$name;
+          $_SESSION['id']=$id;
+          $_SESSION['type']= $type;
+
+          header("location:home.php");
+        }
+      else
+          echo '<script>alert("Login failed");</script>';
+      }
+  }
+  ?>
 <html lang="en" >
 <head>
   <title>Login</title>
@@ -194,40 +225,6 @@ img{
   <input type="password" placeholder='••••••••••••••' class='password' name='pass' required><br>
   <button type="submit" class='btn-login' id='do-login' name='submit'>Login</button>
     </form>
-
-
-
-    <?php
-    // PHP CODE FOR LOGIN VALIDATION
-      if (isset($_POST['submit'])) {
-        require('essentials/_conn.php');
-
-        $email=$_POST['email'];
-        $pass=$_POST['pass'];
-
-        $query="SELECT * from login where email='$email' and pass='$pass' and type ='0'";
-        $res=mysqli_query($conn, $query);
-
-        if ($res) {
-          if(mysqli_num_rows($res)>0)
-          {
-            $row = mysqli_fetch_assoc($res);
-            $name=$row['name'];
-            $id=$row['id'];
-            $type=$row['type'];
-            session_start();
-            $_SESSION['loggedin']=true;
-            $_SESSION['name']=$name;
-            $_SESSION['id']=$id;
-            $_SESSION['type']= $type;
-
-            header("location:home.php");
-          }
-		    else
-			      echo '<script>alert("Login failed");</script>';
-        }
-		}
-    ?>
 
   </div>
 </section>
