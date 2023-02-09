3f9d318ad2489d6ee88794687e7a925163366c7d
Sharat <99925941+sharat0@users.noreply.github.com>
2023-02-09 18:25:57 +0530

    Add files via upload

--- score.php
+++ score.php
@@ -1,6 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
+    <?php
+    session_start();
+    if (!isset($_SESSION['loggedin']) || ($_SESSION['loggedin'])!= true) {
+        header('location: index.php');
+        exit;
+    }
+    
+    if($_SESSION['type'] == 0){
+        header('location: home.php');
+        exit;
+    }
+    ?>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
